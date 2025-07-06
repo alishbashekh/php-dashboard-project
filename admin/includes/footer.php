97309e048d4295816809cc20daae4e1e868f765f
Alishba shekh <alishbashabbir890@gmail.com>
2025-07-06 04:41:30 -0700

    ..

--- admin/includes/footer.php
+++ admin/includes/footer.php
@@ -6,9 +6,8 @@
                 © <script>
                   document.write(new Date().getFullYear())
                 </script>,
-                made with <i class="fa fa-heart"></i> by
-                <a href="https://www.creative-tim.com" class="font-weight-bold" target="_blank">Creative Tim</a>
-                for a better web.
+                developed by ALISHBA SHABBIR
+                
               </div>
             </div>
             <div class="col-lg-6">
