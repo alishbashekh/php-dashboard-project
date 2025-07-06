0abcf64981b2e5b28776db55758da22c6f46c300
Alishba shekh <alishbashabbir890@gmail.com>
2025-07-06 05:09:04 -0700

    ..

--- admin/user-create.php
+++ admin/user-create.php
@@ -5,7 +5,7 @@
         <div class="card">
             <div class="card-header">
                 <h4>add user
-                <a href="user-read.php" class="btn btn-danger float-end">back</a>
+                <a href="user.php" class="btn btn-danger float-end">back</a>
                 </h4>
             </div>
             <div class="card-body">
